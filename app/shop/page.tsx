a8fe8bb866976f0bb13ee27feb137f828da01653
JohnCodes <engrlevijohn@gmail.com>
2024-10-03 11:37:06 +0100

    fix:Testimonial card 2

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 import Wrapper from "@/components/Wrapper";
 import Image from "next/image";
 import Link from "next/link";
-import TestimonialCard from "@/components/testimonialCard";
+import TestimonialCard from "@/components/TestimonialCard";
 
 const Shop = () => {
   const originalPrice = 50;
